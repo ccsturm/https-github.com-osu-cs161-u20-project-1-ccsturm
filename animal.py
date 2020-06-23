a292971a5fd36f1f194da6e15c824dd2109874c4
ccsturm <67247939+ccsturm@users.noreply.github.com>
2020-06-22 22:12:21 -0700

    assignment one completed

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Curt Sturm
+# Date: 6/22/2020
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
+
